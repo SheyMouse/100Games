bfd9c163e5e27adafe25b930cb4199597c4e5bb8
Shey <sheymouse@gmail.com>
2013-10-08 17:12:09 +0100

    greeting
    The first game of the project.
    A very simple ask a person their name, and greet them.
    
    May add some fun extras at bit later

---
+++ greeting.py
@@ -0,0 +1,18 @@
+#
+#               100 Games Project 
+#
+# Game #1: Greeting
+#
+# Ask a person's name and greet them
+# Not really a game but a place to start
+#
+# Inspired by Jennifer Dewalt's excellent 180 websites in 180 days
+# http://jenniferdewalt.com/
+#
+###################################################################
+
+
+print ("Hi! What is your name? ")
+name = input(">>> ")
+
+print ("Welome " + name + "! I hope you enjoy your time browsing the 100 Games project.")
