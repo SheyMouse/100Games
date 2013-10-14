deb86899dad85cf45b18245ea8688daa232d7311
Shey <sheymouse@gmail.com>
2013-10-14 22:18:13 +0100

    It works! But is fragile
    A working version of game 002.  There are two major issues:
    1. There is no handling for anything other than round numbers
    2. There is no handling for 0 or alpha

--- in_progress/100Games_002_YourInterplanetaryAge.py
+++ in_progress/100Games_002_YourInterplanetaryAge.py
@@ -20,6 +20,18 @@
 #
 ###############################################################################
 import time
+from decimal import Decimal
+
+### a year for each planet ###
+mercury_year = 87.97 # days
+venus_year = 224.7 #days
+earth_year = 365.25 #days
+mars_year = 1.88 #years
+jupiter_year = 11.86 # years
+saturn_year = 29.45 #years
+uranus_year = 84.01 # years
+neptune_year = 164.79 # years
+pluto_year = 248.59 # years
 
 print ("Hello again! Welcome to the next game in the 100 Games Project")
 print ("This time we're going to calculate your age in days and years on other planets in the solar system")
@@ -28,15 +40,27 @@ time.sleep(1)
 print("What is your age?")
 age = int(input(">>> "))
 
-
+mercury_age_years = round((age * (earth_year / 87.97)),2)
+venus_age_years = round((age * (earth_year / 224.7)),2)
+earth_age_years = round(age,2)
+mars_age_years = round(age / 1.88 ,2)
+jupiter_age_years = round(age / 11.86 ,2)
+saturn_age_years = round(age / 29.46 ,2)
+uranus_age_years = round(age / 84.01 ,2)
+neptune_age_years = round(age / 164.79 ,2)
+pluto_age_years = round(age / 248.59 ,2)
 
 time.sleep(0.5)
-print("According to my calculation you are:")
-time.sleep(1)
 print()
-print(str(days) + " days old")
-print(str(hours) + " houts old")
-print(str(minutes) + " minutes old")
-print(str(seconds) + " seconds old")
+print("You are " + str(mercury_age_years) + " Mercury years old")
+print("You are " + str(venus_age_years) + " Venus years old")
+print("You are " + str(earth_age_years) + " Earth years old")
+print("You are " + str(mars_age_years) + " Mars years old old")
+print("You are " + str(jupiter_age_years) + " Jupiter years old old")
+print("You are " + str(saturn_age_years) + " Saturn years old old")
+print("You are " + str(uranus_age_years) + " Uranus years old old")
+print("You are " + str(neptune_age_years) + " Neptune years old old")
+print("You are " + str(pluto_age_years) + " Pluto years old old")
+
 
 
